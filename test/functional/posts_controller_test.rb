e3fc0ce20f6397f2e25fac4d74277c344fd23303
Bob Martens <boblmartens@gmail.com>
2008-10-12 00:31:49 -0500

    added functional test; need to add SHOW to POST controller

--- test/functional/posts_controller_test.rb
+++ test/functional/posts_controller_test.rb
@@ -1,8 +1,32 @@
 require 'test_helper'
 
 class PostsControllerTest < ActionController::TestCase
-  # Replace this with your real tests.
-  def test_truth
-    assert true
-  end
+	def test_should_show_index
+		get :index
+		assert_response :success
+		assert_template 'index'
+		assert_not_nil assigns(:posts)
+	end
+
+	def test_should_show_new
+		get :new
+		assert_response :success
+		assert_template 'new'
+		assert_not_nil assigns(:post)
+	end
+
+	def test_should_show_new_form
+		get :new
+		assert_select 'form p', :count => 2
+	end
+
+	def test_should_show_story
+		post :create, :post => {
+			:title => 'test post',
+			:body => 'this is the test post body'}
+		assert ! assigns(:post).new_record?
+		assert_redirected_to post_path
+		assert_not_nil flash[:notice]
+	end
+
 end
