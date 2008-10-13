5f257467ac2b475f9853925d870c0d3cde9cd299
Bob Martens <boblmartens@gmail.com>
2008-10-12 21:19:52 -0500

    added unit tests for POST validations

--- test/unit/post_test.rb
+++ test/unit/post_test.rb
@@ -8,4 +8,18 @@ class PostTest < ActiveSupport::TestCase
 		assert p.valid?
 	end
 
+	def test_should_not_create_without_title
+		p = Post.create(
+			:title => nil,
+			:body => 'failed test')
+		assert p.errors.on(:title)
+	end
+
+	def test_should_not_create_without_body
+		p = Post.create(
+			:title => 'title',
+			:body => nil)
+		assert p.errors.on(:body)
+	end
+
 end
