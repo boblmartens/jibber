6415a2eb67893af3f9dac72ac832c0fc372a28a5
Bob Martens <boblmartens@gmail.com>
2008-10-16 11:38:50 -0500

    added some model specs for POST

---
+++ spec/models/post_spec.rb
@@ -0,0 +1,24 @@
+require File.expand_path(File.dirname(__FILE__) + '/../spec_helper')
+
+describe Post do
+  before(:each) do
+    @valid_attributes = {
+      :title => "value for title",
+      :body => "value for body"
+    }
+  end
+
+  it "should create a new instance given valid attributes" do
+		Post.new(@valid_attributes).should be_valid
+  end
+
+	it "should not save instance if given only body" do
+		Post.new(:body => "body test").should_not be_valid
+	end
+
+	it "should update attributes after edit" do
+		p = Post.create(@valid_attributes)
+		p.update_attributes(:title => "test title")
+		p.title.should == "test title"
+	end
+end
