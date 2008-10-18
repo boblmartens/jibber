d67b9ac5daebb3449a4b7a8b3152db76b7ad8eb7
Bob Martens <boblmartens@gmail.com>
2008-10-18 17:37:43 -0500

    creation of COMMENT model; inclusion of COMMENT spec

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20081009005706) do
+ActiveRecord::Schema.define(:version => 20081018223007) do
+
+  create_table "comments", :force => true do |t|
+    t.text     "body"
+    t.integer  "post_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "posts", :force => true do |t|
     t.string   "title"
