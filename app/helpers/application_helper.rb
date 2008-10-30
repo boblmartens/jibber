e1efb780fd0269aab8347ba5b1fec059aa831376
Bob Martens <boblmartens@gmail.com>
2008-10-30 00:05:46 -0500

    changed from BlueCloth to RDiscount for Markdown formatting

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,9 @@
 # Methods added to this helper will be available to all templates in the application.
+require 'rdiscount'
+
 module ApplicationHelper
+def markdown(text)
+	text.blank? ? "" : RDiscount.new(text).to_html
+end
+
 end
