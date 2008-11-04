2576bac2fa55074b231c8e29f2244d736cfe8b11
Bob Martens <boblmartens@gmail.com>
2008-11-03 23:54:22 -0600

    USER scaffold creation

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :users
+
   map.resources :comments
 
 	map.root :controller => 'posts', :action => 'index' 
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20081020031840) do
+ActiveRecord::Schema.define(:version => 20081104055310) do
 
   create_table "comments", :force => true do |t|
     t.integer  "post_id"
@@ -21,6 +21,14 @@ ActiveRecord::Schema.define(:version => 20081020031840) do
   create_table "posts", :force => true do |t|
     t.string   "title"
     t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "email"
+    t.string   "password"
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
