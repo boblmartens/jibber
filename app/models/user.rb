0a05fc60c0a7083a51da3aacf86bdb8b23f99c48
Bob Martens <boblmartens@gmail.com>
2008-11-14 11:21:56 -0600

    added comments for USER model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ require 'digest/sha2'
 class User < ActiveRecord::Base
   attr_accessor :password
 
+  # receives password input from form and runs through to hash password and create salt
   def password= (pass)
     @password = pass
     self.password_salt = User.random_string(10) if !self.password_salt
@@ -10,11 +11,13 @@ class User < ActiveRecord::Base
   end
 
   protected
-    
+  
+  # takes password and generated salt and hashes it to prepare for input into database
   def self.hash_password(pass, salt)
     Digest::SHA256.hexdigest(pass + salt)
   end
 
+  # creates a random string of integers and letters
   def self.random_string(len)
    chars = ("a".."z").to_a + ("A".."Z").to_a + ("0".."9").to_a
    newpass = ""
