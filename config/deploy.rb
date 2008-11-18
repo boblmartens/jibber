b6876bfba78e3161af3e194d4be45370bf53d8b5
Bob Martens <boblmartens@gmail.com>
2008-11-18 12:02:52 -0600

    changes made to deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,15 +1,30 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application, "jibber"
 
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
 # via the :deploy_to variable:
-# set :deploy_to, "/var/www/#{application}"
+set :deploy_to, "/home/bob/public_html/#{application}"
 
 # If you aren't using Subversion to manage your source code, specify
 # your SCM below:
-# set :scm, :subversion
+set :scm, :git
+set :repository, "git@github.com:boblmartens/jibber.git"
+set :branch, "master"
 
-role :app, "your app-server here"
-role :web, "your web-server here"
-role :db,  "your db-server here", :primary => true
+set :user, 'bob'
+
+role :app, "bobmartens.net"
+role :web, "bobmartens.net"
+role :db,  "bobmartens.net", :primary => true
+
+namespace :deploy do
+  desc "Restarting mod_rails with restart.txt"
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "touch #{current_path}/tmp/restart.txt"
+  end
+
+  [:start, :stop].each do |t|
+    desc "#{t} task is a no-op with mod_rails"
+    task t, :roles => :app, do ; end
+  end
+end
