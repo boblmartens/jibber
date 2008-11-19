d7d0a113440c2b9101aa710e7c42d2e407e15c70
Bob Martens <boblmartens@gmail.com>
2008-11-18 21:18:51 -0600

    small change

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ set :application, "jibber"
 # If you aren't deploying to /u/apps/#{application} on the target
 # servers (which is the default), you can specify the actual location
 # via the :deploy_to variable:
-set :deploy_to, "/home/bob/app/#{application}"
+set :deploy_to, "/home/bob/apps/#{application}"
 
 # If you aren't using Subversion to manage your source code, specify
 # your SCM below:
