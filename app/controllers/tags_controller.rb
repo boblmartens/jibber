a9787a1d2e46b809231c49392b10dc093eb47d31
Bob Martens <boblmartens@gmail.com>
2008-11-28 00:02:27 -0600

    starting work on tag cloud

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,4 +1,9 @@
 class TagsController < ApplicationController
+  def index
+    @tags = Tag.find(:all)
+    @css = ['css1', 'css2']
+  end
+
   def show
     @posts = Post.find_tagged_with(params[:id])
   end
