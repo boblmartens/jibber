1c4ac03bd300a3b46052d064df0191e1eb87a29c
Bob Martens <boblmartens@gmail.com>
2008-12-02 16:34:39 -0600

    finally rid of rspec and first tests written

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,8 +1,14 @@
 require 'test_helper'
 
-class TestTest < ActiveSupport::TestCase
+class UserTest < ActiveSupport::TestCase
   # Replace this with your real tests.
   test "the truth" do
     assert true
+  end
+
+  test "user name saved" do
+    u = User.create(:email => 'boblmartens@gmail.com', :name => 'bob')
+    assert u.valid?
+    assert_equal users(:one).name, 'bob'
   end
 end
