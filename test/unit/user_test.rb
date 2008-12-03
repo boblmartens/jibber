e1ffcbfb581ccdc7416ea32a07600d106a5963d7
Bob Martens <boblmartens@gmail.com>
2008-12-03 00:26:11 -0600

    moving to Shoulda for testing

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -11,4 +11,24 @@ class UserTest < ActiveSupport::TestCase
     assert u.valid?
     assert_equal users(:one).name, 'bob'
   end
+
+  should "be true" do
+    assert true
+  end
+
+  context "a user" do
+    setup do
+      @user = User.new
+      @user.email = 'test@test.com'
+      @user.name = 'Test'
+      @user.password = 'test'
+      @user.save
+    end
+
+    should "have valid attributes" do
+      assert_equal @user.name, 'Test'
+      assert_equal @user.email, 'test@test.com'
+    end
+
+  end
 end
