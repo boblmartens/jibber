c08914977c0bf5318bf588ee0065b6cb79522a04
Bob Martens <boblmartens@gmail.com>
2008-12-06 01:23:49 -0600

    acts-as-taggable-on now working as as gem

--- config/environment.rb
+++ config/environment.rb
@@ -28,6 +28,7 @@ Rails::Initializer.run do |config|
     config.gem "thoughtbot-shoulda", :lib => "shoulda/rails", :source => "http://gems.github.com"
     config.gem "rdiscount"
     config.gem "mislav-will_paginate", :lib => "will_paginate", :source => "http://gems.github.com"
+    config.gem "mbleigh-acts-as-taggable-on", :source => "http://gems.github.com", :lib => "acts-as-taggable-on"
 
   # Only load the plugins named here, in the order given. By default, all plugins 
   # in vendor/plugins are loaded in alphabetical order.
