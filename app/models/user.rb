87111d3315c1030a6c9ae01fd581b3b1cd10b4da
Bob Martens <boblmartens@gmail.com>
2008-12-05 18:54:51 -0600

    user must now duplicate password for both creating a new user and editing a user

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,10 @@ class User < ActiveRecord::Base
   has_many :comments
 
 # validates_presence_of   :password, :on => 'create'
-  validates_uniqueness_of :email
-  validates_presence_of   :email
-  validates_presence_of   :name
+  validates_uniqueness_of   :email
+  validates_presence_of     :email
+  validates_presence_of     :name
+  validates_confirmation_of :password
 
   attr_protected  :admin
 
