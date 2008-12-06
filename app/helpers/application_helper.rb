b3ed5276c36e35e03f70a6c6dd032d11e68839ab
Bob Martens <boblmartens@gmail.com>
2008-12-06 17:51:43 -0600

    deletion of comments only available from admin

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,20 @@ def markdown(text)
 	text.blank? ? "" : RDiscount.new(text).to_html
 end
 
+def admin?
+  if @current_user.admin == true
+    return true
+  else
+    return false
+  end
+end
+
+def user?
+  if @current_user
+    return true
+  else
+    return false
+  end
+end
+
 end
