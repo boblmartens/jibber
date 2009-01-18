957c9b60ca55006d8e4c1e838c3055ad567bf2aa
Bob Martens <boblmartens@gmail.com>
2009-01-18 00:48:00 -0600

    initial inclusion of Gravatar functionality

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,16 +1,9 @@
 require 'rdiscount'
+require 'digest/md5' 
 
 module ApplicationHelper
 def markdown(text)
 	text.blank? ? "" : RDiscount.new(text).to_html
-end
-
-def admin?
-  if @current_user.admin == true
-    return true
-  else
-    return false
-  end
 end
 
 def user?
@@ -19,6 +12,14 @@ def user?
   else
     return false
   end
+end
+
+def gravatar_url_for(email, options = {})
+  url_for({ :gravatar_id => Digest::MD5.hexdigest(email), 
+            :host => 'www.gravatar.com',
+            :protocol => 'http://', 
+            :only_path => false, 
+            :controller => 'avatar.php'}.merge(options))  
 end
 
 end
