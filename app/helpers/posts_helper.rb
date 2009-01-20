6ed57628491292c5b33d2b3e9f156a47a301ab05
Bob Martens <boblmartens@gmail.com>
2009-01-20 00:00:58 -0600

    tag list when showing posts now works properly; dynamically changes with number of tags

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,17 @@
 module PostsHelper
+
+def commaed_tags
+  @tags = ""
+  @post.tags.each do |tag|
+    if tag.name == @post.tags.first.name
+      @tags << link_to(tag.name, tag_path(:id => tag.name))
+    elsif tag.name != @post.tags.last.name
+      @tags << ", " + link_to(tag.name, tag_path(:id => tag.name)) 
+    else 
+      @tags << " and " + link_to(tag.name, tag_path(:id => tag.name))
+    end
+  end
+
+  @tags
+end
 end
