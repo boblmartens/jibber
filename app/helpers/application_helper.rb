5f95c3c5b8ea6d005b7ddffc1914a1813c933002
Bob Martens <boblmartens@gmail.com>
2009-01-20 20:56:21 -0600

    moved commaed_tags to Application Helper and some updates to styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -22,4 +22,18 @@ def gravatar_url_for(email, options = {})
             :controller => 'avatar.php'}.merge(options))  
 end
 
+def commaed_tags(post)
+  @tags = ""
+  post.tags.each do |tag|
+    if tag.name == post.tags.first.name
+      @tags << link_to(tag.name, tag_path(:id => tag.name))
+    elsif tag.name != @post.tags.last.name
+      @tags << ", " + link_to(tag.name, tag_path(:id => tag.name)) 
+    else 
+      @tags << " and " + link_to(tag.name, tag_path(:id => tag.name))
+    end
+  end
+
+  @tags
+end
 end
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,17 +1,2 @@
 module PostsHelper
-
-def commaed_tags(post)
-  @tags = ""
-  post.tags.each do |tag|
-    if tag.name == post.tags.first.name
-      @tags << link_to(tag.name, tag_path(:id => tag.name))
-    elsif tag.name != @post.tags.last.name
-      @tags << ", " + link_to(tag.name, tag_path(:id => tag.name)) 
-    else 
-      @tags << " and " + link_to(tag.name, tag_path(:id => tag.name))
-    end
-  end
-
-  @tags
-end
 end
